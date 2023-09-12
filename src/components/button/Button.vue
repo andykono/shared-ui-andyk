283a05f18d2b40ffb29bf9c0ae57afeda8ff1181
AndyKIron <andy.kononenko@unity3d.com>
2023-09-12 10:13:02 +0300

    feat: in process

--- src/components/button/Button.vue
+++ src/components/button/Button.vue
@@ -1,10 +1,9 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }} </button>
+  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
 </template>
 
 <script lang="ts" setup>
-import './button.css';
-import { computed } from 'vue';
+import {computed} from 'vue';
 
 const props = withDefaults(defineProps<{
   /**
@@ -24,7 +23,7 @@ const props = withDefaults(defineProps<{
    */
   backgroundColor?: string,
 
-}>(), { primary: false });
+}>(), {primary: false});
 
 const emit = defineEmits<{
   (e: 'click', id: number): void;
@@ -46,3 +45,42 @@ const onClick = () => {
 };
 
 </script>
+
+<style scoped lang="scss">
+.storybook-button {
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+}
+
+.storybook-button--primary {
+  color: white;
+  background-color: #1ea7fd;
+}
+
+.storybook-button--secondary {
+  color: #333;
+  background-color: transparent;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+}
+
+.storybook-button--small {
+  font-size: 12px;
+  padding: 10px 16px;
+}
+
+.storybook-button--medium {
+  font-size: 14px;
+  padding: 11px 20px;
+}
+
+.storybook-button--large {
+  font-size: 16px;
+  padding: 12px 24px;
+}
+
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,50 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
+import {resolve} from 'node:path'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 import alias from './aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias,
-  },
-  build:{
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'shared-ui-andyk',
-      formats: ['es'],
+    plugins: [
+        vue(),
+        dts(),
+        cssInjectedByJsPlugin(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: './package.json',
+                    dest: './',
+                },
+                {
+                    src: './README.md',
+                    dest: './',
+                }
+            ],
+        }),],
+    resolve: {
+        alias,
     },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        preserveModules: true,
-        entryFileNames: '[name].vue.js',
-        assetFileNames: 'assets/[name].[ext]',
-        globals: {
-          vue: 'Vue',
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'shared-ui-andyk',
+            formats: ['es'],
         },
-      },
-    },
-  }
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                preserveModules: true,
+                entryFileNames: '[name].vue.js',
+                assetFileNames: 'assets/[name].[ext]',
+                globals: {
+                    vue: 'Vue',
+                },
+            },
+        },
+        target: 'esnext',
+        cssCodeSplit: true,
+    }
 })
